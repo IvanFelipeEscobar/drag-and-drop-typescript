f8d31ebf396c1cf351b49b56dbb2fecfbf23de1b
Ivan Escobar <ivnescbr1989@gmail.com>
2023-06-25 09:35:54 -0400

    form handle logic

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,16 @@
+function binder(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDesc: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFunc = originalMethod.bind(this);
+      return boundFunc;
+    },
+  };
+
+  return adjDesc;
+}
+
 class ProjectInput {
   templateEl: HTMLTemplateElement;
   hostEl: HTMLDivElement;
@@ -15,20 +28,37 @@ class ProjectInput {
     this.element = importHtmlNode.firstElementChild as HTMLFormElement;
     this.element.id = `user-input`;
     this.titleEl = this.element.querySelector(`#title`) as HTMLInputElement;
-    this.descEl = this.element.querySelector(`#description`) as HTMLInputElement;
+    this.descEl = this.element.querySelector(
+      `#description`
+    ) as HTMLInputElement;
     this.peopleEl = this.element.querySelector(`#people`) as HTMLInputElement;
-    
+
     this.configure();
     this.addEl();
   }
-
-  private submitHandler(e: Event){
+  private userInput(): [string, string, number] | void{
+    const enterTitle = this.titleEl.value;
+    const enterDesc = this.descEl.value;
+    const enterPeople = this.peopleEl.value;
+    if(enterTitle.trim().length === 0 || enterDesc.trim().length === 0 || enterPeople.trim().length){
+        alert(`Invalid input! Please try again!`)
+        return
+    } else {
+        return [enterTitle, enterDesc, +enterPeople]
+    }
+  }
+  @binder
+  private submitHandler(e: Event) {
     e.preventDefault();
-    console.log(this.titleEl.value)
+    console.log(this.titleEl.value);
+    const newInput = this.userInput()
+    if(Array.isArray(newInput)){
+        
+    }
   }
 
   private configure() {
-    this.element.addEventListener(`submit`, this.submitHandler.bind(this));
+    this.element.addEventListener(`submit`, this.submitHandler);
   }
 
   private addEl() {
