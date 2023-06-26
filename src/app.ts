9966c306d7d16a3ff729ece11f5ff9450d3744ea
Ivan Escobar <ivnescbr1989@gmail.com>
2023-06-25 21:54:49 -0400

    Project list render setup

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,31 @@
-function binder(_: any, _2: string, descriptor: PropertyDescriptor) {
+interface ValidInput {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+const validate = (inputToValidate: ValidInput) => {
+    let isValid = true;
+    if(inputToValidate.required) {
+        isValid = isValid && inputToValidate.value.toString().trim().length != 0
+    }
+    if (inputToValidate.minLength != null && typeof inputToValidate.value === `string`){
+        isValid = isValid && inputToValidate.value.length >= inputToValidate.minLength
+    }
+    if (inputToValidate.maxLength != null && typeof inputToValidate.value === `string`){
+        isValid = isValid && inputToValidate.value.length <= inputToValidate.maxLength
+    }
+    if(inputToValidate.min != null && typeof inputToValidate.value === `number`){
+        isValid = isValid && inputToValidate.value >= inputToValidate.min
+    }
+    if(inputToValidate.max != null && typeof inputToValidate.value === `number`){
+        isValid = isValid && inputToValidate.value <= inputToValidate.max
+    }
+    return isValid
+}
+const binder = (_: any, _2: string, descriptor: PropertyDescriptor) => {
   const originalMethod = descriptor.value;
   const adjDesc: PropertyDescriptor = {
     configurable: true,
@@ -10,7 +37,30 @@ function binder(_: any, _2: string, descriptor: PropertyDescriptor) {
 
   return adjDesc;
 }
-
+class ProjectList {
+    templateEl: HTMLTemplateElement;
+    hostEl: HTMLDivElement;
+    element: HTMLElement;
+    constructor(private type: `active` | `finished`){
+        this.templateEl = document.getElementById(
+            `project-list`
+          )! as HTMLTemplateElement;
+          this.hostEl = document.getElementById(`app`)! as HTMLDivElement;
+          const importHtmlNode = document.importNode(this.templateEl.content, true);
+          this.element = importHtmlNode.firstElementChild as HTMLElement;
+          this.element.id = `${type}-projects`;
+          this.attachEl()
+          this.renderProject()
+    }
+private renderProject(){
+    const listId = `${this.type}-projects-list`;
+    this.element.querySelector(`ul`)!.id = listId;
+    this.element.querySelector(`h2`)!.textContent = this.type.toUpperCase() + `PROJECTS`
+}
+    private attachEl() {
+    this.hostEl.insertAdjacentElement(`beforeend`, this.element);
+    }
+}
 class ProjectInput {
   templateEl: HTMLTemplateElement;
   hostEl: HTMLDivElement;
@@ -36,24 +86,59 @@ class ProjectInput {
     this.configure();
     this.addEl();
   }
-  private userInput(): [string, string, number] | void{
+  private userInput(): [string, string, number] | void {
     const enterTitle = this.titleEl.value;
     const enterDesc = this.descEl.value;
     const enterPeople = this.peopleEl.value;
-    if(enterTitle.trim().length === 0 || enterDesc.trim().length === 0 || enterPeople.trim().length){
-        alert(`Invalid input! Please try again!`)
-        return
+
+const titleValidation: ValidInput = {
+    value: enterTitle,
+    required: true,
+    maxLength: 25,
+    minLength: 3
+}
+const descValidation: ValidInput = {
+    value: enterDesc,
+    required: true,
+    maxLength: 100,
+    minLength: 3
+}
+const peopleValidation: ValidInput = {
+    value: +enterPeople,
+    required: true,
+    max: 10,
+    min: 1
+}
+
+    if (
+    //   enterTitle.trim().length === 0 ||
+    //   enterDesc.trim().length === 0 ||
+    //   enterPeople.trim().length === 0
+    !validate(titleValidation) ||
+    !validate(descValidation) ||
+    !validate(peopleValidation)
+    ) {
+      alert(`Invalid input! Please try again!`);
+      return;
     } else {
-        return [enterTitle, enterDesc, +enterPeople]
+      return [enterTitle, enterDesc, +enterPeople];
     }
   }
+  private clearInput() {
+    this.titleEl.value = ``;
+    this.descEl.value = ``;
+    this.peopleEl.value = ``;
+  }
+  
   @binder
   private submitHandler(e: Event) {
     e.preventDefault();
-    console.log(this.titleEl.value);
-    const newInput = this.userInput()
-    if(Array.isArray(newInput)){
-        
+    // console.log(this.titleEl.value);
+    const newInput = this.userInput();
+    if (Array.isArray(newInput)) {
+      const [title, desc, people] = newInput;
+      // console.log(title, desc, people)
+      this.clearInput();
     }
   }
 
@@ -67,3 +152,5 @@ class ProjectInput {
 }
 
 const newProject = new ProjectInput();
+const activeProjects = new ProjectList(`active`);
+const finishedProjects = new ProjectList(`finished`);
